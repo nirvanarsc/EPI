0a840c1c68d03bd5a21fcf5839dd86d70f2bcb61
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-08 15:46:41 +0900

    Chapter 5 Problem 4

---
+++ epi/Chapter5/ClosestIntSameWeight.java
@@ -0,0 +1,29 @@
+package epi.Chapter5;
+
+import epi.TestRunner;
+import epi.test_framework.EpiTest;
+
+public final class ClosestIntSameWeight {
+
+    @EpiTest(testDataFile = "closest_int_same_weight.tsv")
+    public static long closestIntSameBitCount(long x) {
+        int i = 0;
+
+        while (isBitSet(x, i) == isBitSet(x, i + 1)) {
+            i++;
+        }
+
+        return SwapBits.swapBits(x, i + 1, i);
+    }
+
+    private static boolean isBitSet(long x, int idx) {
+        return (x & (1L << idx)) > 0;
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(args, "epi.Chapter5.ClosestIntSameWeight");
+    }
+
+    private ClosestIntSameWeight() {
+    }
+}
