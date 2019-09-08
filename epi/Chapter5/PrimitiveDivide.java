2f7b724bb368bcd888faafa8f50de5fc691c7e6a
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-08 22:26:30 +0900

    Chapter 5 Problem 6

--- epi/Chapter5/PrimitiveDivide.java
+++ epi/Chapter5/PrimitiveDivide.java
@@ -3,15 +3,27 @@ package epi.Chapter5;
 import epi.TestRunner;
 import epi.test_framework.EpiTest;
 
-public class PrimitiveDivide {
+public final class PrimitiveDivide {
 
     @EpiTest(testDataFile = "primitive_divide.tsv")
-    public static int divide(int x, int y) {
-        // TODO - you fill in here.
-        return 0;
+    public static int divide(long x, long y) {
+        int result = 0, power = 32;
+        long yPower = y << power;
+        while (x >= y) {
+            while (yPower > x) {
+                yPower >>>= 1;
+                --power;
+            }
+            result += 1L << power;
+            x -= yPower;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         TestRunner.run(args, "epi.Chapter5.PrimitiveDivide");
+    }
+
+    private PrimitiveDivide() {
     }
 }
