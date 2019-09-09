9a8c224100547fdc0e0bd3451c6e105124d5136f
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-10 08:52:40 +0900

    Chapter 5 Problem 7

--- epi/Chapter5/PowerXY.java
+++ epi/Chapter5/PowerXY.java
@@ -3,14 +3,30 @@ package epi.Chapter5;
 import epi.TestRunner;
 import epi.test_framework.EpiTest;
 
-public class PowerXY {
+public final class PowerXY {
 
     @EpiTest(testDataFile = "power_x_y.tsv")
     public static double power(double x, int y) {
-        return Math.pow(x, y);
+        if (y < 0) {
+            y = -y;
+            x = 1.0 / x;
+        }
+        short idx = 0;
+        double res = 1;
+        while (idx < Integer.SIZE) {
+            if (((y >> idx) & 1) == 1) {
+                res *= x;
+            }
+            x *= x;
+            idx++;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
         TestRunner.run(args, "epi.Chapter5.PowerXY");
+    }
+
+    private PowerXY() {
     }
 }
