4d1614640277e32878ebee8987a609933e85a13b
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-10 10:25:12 +0900

    Chapter 5 Problem 9

--- epi/Chapter5/IsNumberPalindromic.java
+++ epi/Chapter5/IsNumberPalindromic.java
@@ -3,15 +3,35 @@ package epi.Chapter5;
 import epi.TestRunner;
 import epi.test_framework.EpiTest;
 
-public class IsNumberPalindromic {
+public final class IsNumberPalindromic {
 
     @EpiTest(testDataFile = "is_number_palindromic.tsv")
     public static boolean isPalindromeNumber(int x) {
-        // TODO - you fill in here.
+        return x < 0 ? false : ReverseDigits.reverse(x) == x;
+    }
+
+    @EpiTest(testDataFile = "is_number_palindromic.tsv")
+    public static boolean isPalindromeNumber2(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        final short length = (short) Math.log10(x);
+        int msdMask = (int) Math.pow(10, length);
+
+        for (int i = 0; 2 * i < length; x %= msdMask, x /= 10, msdMask /= 100, i++) {
+            if (x / msdMask != x % 10) {
+                return false;
+            }
+        }
+
         return true;
     }
 
     public static void main(String[] args) {
         TestRunner.run(args, "epi.Chapter5.IsNumberPalindromic");
+    }
+
+    private IsNumberPalindromic() {
     }
 }
