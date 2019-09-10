32d41e5f05f26d83c3dd9240f7f76cf9c357a859
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-10 09:09:34 +0900

    Chapter 5 Problem 8

--- epi/Chapter5/ReverseDigits.java
+++ epi/Chapter5/ReverseDigits.java
@@ -3,15 +3,25 @@ package epi.Chapter5;
 import epi.TestRunner;
 import epi.test_framework.EpiTest;
 
-public class ReverseDigits {
+public final class ReverseDigits {
 
     @EpiTest(testDataFile = "reverse_digits.tsv")
     public static long reverse(int x) {
-        // TODO - you fill in here.
-        return 0;
+        long reversed = 0;
+        int absX = Math.abs(x);
+        int length = (int) Math.log10(absX);
+        while (length >= 0) {
+            final int digit = absX % 10;
+            reversed += digit * Math.pow(10, length--);
+            absX /= 10;
+        }
+        return x < 0 ? -reversed : reversed;
     }
 
     public static void main(String[] args) {
         TestRunner.run(args, "epi.Chapter5.ReverseDigits");
+    }
+
+    private ReverseDigits() {
     }
 }
