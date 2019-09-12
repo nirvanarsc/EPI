b3b65ac433a89233ee04b684cfcba9ff4b4f6642
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-12 21:08:19 +0900

    Chapter 5 Problem 11

--- epi/Chapter5/RectangleIntersection.java
+++ epi/Chapter5/RectangleIntersection.java
@@ -4,7 +4,8 @@ import epi.TestRunner;
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 
-public class RectangleIntersection {
+public final class RectangleIntersection {
+
     @EpiUserType(ctorParams = {int.class, int.class, int.class, int.class})
     public static class Rectangle {
         int x, y, width, height;
@@ -55,12 +56,29 @@ public class RectangleIntersection {
     }
 
     @EpiTest(testDataFile = "rectangle_intersection.tsv")
-    public static Rectangle intersectRectangle(Rectangle R1, Rectangle R2) {
-        // TODO - you fill in here.
-        return new Rectangle(0, 0, 0, 0);
+    public static Rectangle intersectRectangle(Rectangle r1, Rectangle r2) {
+        if (!intersects(r1, r2)) {
+            return new Rectangle(0, 0, -1, -1); // No intersection.
+        }
+        return new Rectangle(
+                Math.max(r1.x, r2.x),
+                Math.max(r1.y, r2.y),
+                Math.min(r1.x + r1.width, r2.x + r2.width) - Math.max(r1.x, r2.x),
+                Math.min(r1.y + r1.height, r2.y + r2.height) - Math.max(r1.y, r2.y)
+        );
+    }
+
+    private static boolean intersects(Rectangle r1, Rectangle r2) {
+        return r1.x <= r2.x + r2.width
+                && r1.x + r1.width >= r2.x
+                && r1.y <= r2.y + r2.height
+                && r1.y + r1.height >= r2.y;
     }
 
     public static void main(String[] args) {
         TestRunner.run(args, "epi.Chapter5.RectangleIntersection");
+    }
+
+    private RectangleIntersection() {
     }
 }
