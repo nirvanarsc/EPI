a6003cbe9a0c34ab7a27dee1ccc5274af21003f9
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-13 12:34:32 +0900

    Chapter 6 Problem 2

---
+++ epi/Chapter6/IntAsArrayIncrement.java
@@ -0,0 +1,32 @@
+package epi.Chapter6;
+
+import epi.TestRunner;
+import epi.test_framework.EpiTest;
+
+import java.util.List;
+
+public final class IntAsArrayIncrement {
+
+    @EpiTest(testDataFile = "int_as_array_increment.tsv")
+    public static List<Integer> plusOne(List<Integer> a) {
+        int end = a.size() - 1;
+        while (end >= 0 && a.get(end) == 9) {
+            a.set(end, 0);
+            end--;
+        }
+        if (end == -1) {
+            a.add(0, 1);
+        } else {
+            a.set(end, a.get(end) + 1);
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(args, "epi.Chapter6.IntAsArrayIncrement");
+    }
+
+    private IntAsArrayIncrement() {
+    }
+}
