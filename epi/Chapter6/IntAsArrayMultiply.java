ef2af03af5f5f01c37103c29f4acc9d790965038
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-16 16:03:42 +0900

    Chapter 6 Problem 3
    Book solution

--- epi/Chapter6/IntAsArrayMultiply.java
+++ epi/Chapter6/IntAsArrayMultiply.java
@@ -12,7 +12,7 @@ public final class IntAsArrayMultiply {
     @EpiTest(testDataFile = "int_as_array_multiply.tsv")
     public static List<Integer> multiply(List<Integer> num1, List<Integer> num2) {
         if (num1.get(0) == 0 || num2.get(0) == 0) {
-            return new ArrayList<>(Collections.singleton(0));
+            return Collections.singletonList(0);
         }
         boolean negative = false;
         if (num1.get(0) < 0 ^ num2.get(0) < 0) {
@@ -71,6 +71,34 @@ public final class IntAsArrayMultiply {
         }
 
         return larger;
+    }
+
+    @EpiTest(testDataFile = "int_as_array_multiply.tsv")
+    public static List<Integer> multiply2(List<Integer> num1, List<Integer> num2) {
+        final int sign = num1.get(0) < 0 ^ num2.get(0) < 0 ? -1 : 1;
+        num1.set(0, Math.abs(num1.get(0)));
+        num2.set(0, Math.abs(num2.get(0)));
+
+        List<Integer> res = new ArrayList<>(Collections.nCopies(num1.size() + num2.size(), 0));
+
+        for (int i = num1.size() - 1; i >= 0; --i) {
+            for (int j = num2.size() - 1; j >= 0; --j) {
+                res.set(i + j + 1, res.get(i + j + 1) + num1.get(i) * num2.get(j));
+                res.set(i + j, res.get(i + j) + res.get(i + j + 1) / 10);
+                res.set(i + j + 1, res.get(i + j + 1) % 10);
+            }
+        }
+
+        int first_not_zero = 0;
+        while (first_not_zero < res.size() && res.get(first_not_zero) == 0) {
+            first_not_zero++;
+        }
+        res = res.subList(first_not_zero, res.size());
+        if (res.isEmpty()) {
+            return Collections.singletonList(0);
+        }
+        res.set(0, res.get(0) * sign);
+        return res;
     }
 
     public static void main(String[] args) {
