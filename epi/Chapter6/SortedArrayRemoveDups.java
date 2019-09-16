a8277c22650cd5c001b2f8e33827b886d0bc8e63
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-16 22:08:21 +0900

    Chapter 6 Problem 5

---
+++ epi/Chapter6/SortedArrayRemoveDups.java
@@ -0,0 +1,60 @@
+package epi.Chapter6;
+
+import epi.TestRunner;
+import epi.test_framework.EpiTest;
+import epi.test_framework.TimedExecutor;
+
+import java.util.List;
+
+public final class SortedArrayRemoveDups {
+
+    // Returns the number of valid entries after deletion.
+//    public static int deleteDuplicates(List<Integer> a) {
+//        if (a.isEmpty()) {
+//            return 0;
+//        }
+//        int idx = 1;
+//        for (int i = 0, j = 1; j < a.size(); i++, idx++) {
+//            if (a.get(i).equals(a.get(j))) {
+//                while (j < a.size() && a.get(i).equals(a.get(j))) {
+//                    j++;
+//                }
+//                if (j == a.size()) {
+//                    break;
+//                }
+//                a.set(i + 1, a.get(j));
+//            } else {
+//                j++;
+//            }
+//        }
+//
+//        return idx;
+//    }
+
+    public static int deleteDuplicates(List<Integer> a) {
+        if (a.isEmpty()) {
+            return 0;
+        }
+        int idx = 1;
+        for (int i = 1; i < a.size(); i++) {
+            if (!a.get(idx - 1).equals(a.get(i))) {
+                a.set(idx++, a.get(i));
+            }
+        }
+
+        return idx;
+    }
+
+    @EpiTest(testDataFile = "sorted_array_remove_dups.tsv")
+    public static List<Integer> deleteDuplicatesWrapper(TimedExecutor executor, List<Integer> a) throws Exception {
+        final int end = executor.run(() -> deleteDuplicates(a));
+        return a.subList(0, end);
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(args, "epi.Chapter6.SortedArrayRemoveDups");
+    }
+
+    private SortedArrayRemoveDups() {
+    }
+}
