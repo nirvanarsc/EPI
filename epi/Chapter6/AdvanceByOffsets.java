fbb9c90f797bd59cf0f3668381eaed8b38e81cf9
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-16 19:25:33 +0900

    Chapter 6 Problem 4

---
+++ epi/Chapter6/AdvanceByOffsets.java
@@ -0,0 +1,26 @@
+package epi.Chapter6;
+
+import epi.TestRunner;
+import epi.test_framework.EpiTest;
+
+import java.util.List;
+
+public final class AdvanceByOffsets {
+
+    @EpiTest(testDataFile = "advance_by_offsets.tsv")
+    public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
+        int furthestReach = 0;
+        for (int i = 0; i < maxAdvanceSteps.size() && i <= furthestReach; i++) {
+            furthestReach = Math.max(maxAdvanceSteps.get(i) + i, furthestReach);
+        }
+
+        return furthestReach >= maxAdvanceSteps.size() - 1;
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(args, "epi.Chapter6.AdvanceByOffsets");
+    }
+
+    private AdvanceByOffsets() {
+    }
+}
