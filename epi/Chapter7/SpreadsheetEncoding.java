2a27b0afb25bf6a047600d68229e636adc9721de
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-19 23:20:21 +0900

    Chapter 7 Problem 3

---
+++ epi/Chapter7/SpreadsheetEncoding.java
@@ -0,0 +1,23 @@
+package epi.Chapter7;
+
+import epi.test_framework.EpiTest;
+import epi.utils.TestRunner;
+
+public final class SpreadsheetEncoding {
+
+    @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
+    public static int ssDecodeColID(String col) {
+        int res = 0;
+        for (char c : col.toCharArray()) {
+            res = res * 26 + c - '@';
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(args);
+    }
+
+    private SpreadsheetEncoding() {
+    }
+}
