f1f7bb806e3fa98ba4b9b1d9e1c4898fb7240d4e
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-21 01:21:05 +0900

    Chapter 7 Problem 11

---
+++ epi/Chapter7/SnakeString.java
@@ -0,0 +1,29 @@
+package epi.Chapter7;
+
+import epi.test_framework.EpiTest;
+import epi.utils.TestRunner;
+
+public final class SnakeString {
+
+    @EpiTest(testDataFile = "snake_string.tsv")
+    public static String snakeString(String s) {
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < s.length(); i += 4) {
+            sb.append(s.charAt(i));
+        }
+        for (int i = 0; i < s.length(); i += 2) {
+            sb.append(s.charAt(i));
+        }
+        for (int i = 3; i < s.length(); i += 4) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(args);
+    }
+
+    private SnakeString() {
+    }
+}
