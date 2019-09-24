dd9cec54139dcb4aeb7095d2fb45f5717e87561c
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-25 00:08:29 +0900

    Chapter 9 Problems 6
    Using a Tuple

--- epi/Chapter9/SunsetView.java
+++ epi/Chapter9/SunsetView.java
@@ -8,8 +8,19 @@ import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class SunsetView {
+
+    private static final class Building {
+        public Integer id;
+        public Integer height;
+
+        private Building(Integer id, Integer height) {
+            this.id = id;
+            this.height = height;
+        }
+    }
 
     public static List<Integer> examineBuildingsWithSunset(Iterator<Integer> sequence) {
         final Deque<Integer> buildings = new LinkedList<>();
@@ -18,20 +29,42 @@ public final class SunsetView {
         while (sequence.hasNext()) {
             final Integer curr = sequence.next();
             while (!buildings.isEmpty() && buildings.peekFirst() <= curr) {
-                buildings.pollFirst();
-                res.pollFirst();
+                buildings.removeFirst();
+                res.removeFirst();
             }
             buildings.addFirst(curr);
-            res.addFirst(i);
-            i++;
+            res.addFirst(i++);
         }
 
         return new ArrayList<>(res);
     }
 
+    public static List<Integer> examineBuildingsWithSunset2(Iterator<Integer> sequence) {
+
+        final Deque<Building> buildings = new LinkedList<>();
+        int i = 0;
+        while (sequence.hasNext()) {
+            final Integer curr = sequence.next();
+            while (!buildings.isEmpty() && buildings.peekFirst().height <= curr) {
+                buildings.removeFirst();
+            }
+            buildings.addFirst(new Building(i++, curr));
+        }
+
+        return buildings
+                .stream()
+                .map(building -> building.id)
+                .collect(Collectors.toList());
+    }
+
     @EpiTest(testDataFile = "sunset_view.tsv")
     public static List<Integer> examineBuildingsWithSunsetWrapper(List<Integer> sequence) {
         return examineBuildingsWithSunset(sequence.iterator());
+    }
+
+    @EpiTest(testDataFile = "sunset_view.tsv")
+    public static List<Integer> examineBuildingsWithSunsetWrapper2(List<Integer> sequence) {
+        return examineBuildingsWithSunset2(sequence.iterator());
     }
 
     public static void main(String[] args) {
