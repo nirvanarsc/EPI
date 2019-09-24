a97c1b4cd36557ffc8c8a0cba2d6acad96bb6a62
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-24 23:57:56 +0900

    Chapter 9 Problems 6

--- epi/Chapter9/SunsetView.java
+++ epi/Chapter9/SunsetView.java
@@ -3,14 +3,30 @@ package epi.Chapter9;
 import epi.test_framework.EpiTest;
 import epi.utils.TestRunner;
 
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public final class SunsetView {
 
     public static List<Integer> examineBuildingsWithSunset(Iterator<Integer> sequence) {
-        // TODO - you fill in here.
-        return null;
+        final Deque<Integer> buildings = new LinkedList<>();
+        final Deque<Integer> res = new LinkedList<>();
+        int i = 0;
+        while (sequence.hasNext()) {
+            final Integer curr = sequence.next();
+            while (!buildings.isEmpty() && buildings.peekFirst() <= curr) {
+                buildings.pollFirst();
+                res.pollFirst();
+            }
+            buildings.addFirst(curr);
+            res.addFirst(i);
+            i++;
+        }
+
+        return new ArrayList<>(res);
     }
 
     @EpiTest(testDataFile = "sunset_view.tsv")
