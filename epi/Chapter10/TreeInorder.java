fb5f8f8ddcaffbb7038a172e4b8983ec45f8ed17
nirvanarsc <nirvana_rsc@yahoo.com>
2019-09-28 19:36:22 +0900

    Chapter 10 Problem 7

---
+++ epi/Chapter10/TreeInorder.java
@@ -0,0 +1,56 @@
+package epi.Chapter10;
+
+import epi.BinaryTreeNode;
+import epi.test_framework.EpiTest;
+import epi.utils.TestRunner;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public final class TreeInorder {
+
+    @EpiTest(testDataFile = "tree_inorder.tsv")
+    public static List<Integer> inorderTraversalRecursive(BinaryTreeNode<Integer> tree) {
+        final List<Integer> res = new LinkedList<>();
+        helper(tree, res);
+        return res;
+    }
+
+    static void helper(BinaryTreeNode<Integer> node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        helper(node.left, res);
+        res.add(node.data);
+        helper(node.right, res);
+    }
+
+    @EpiTest(testDataFile = "tree_inorder.tsv")
+    public static List<Integer> inorderTraversalIterative(BinaryTreeNode<Integer> node) {
+        final List<Integer> res = new ArrayList<>();
+        final Deque<BinaryTreeNode<Integer>> s = new LinkedList<>();
+        BinaryTreeNode<Integer> curr = node;
+
+        while (!s.isEmpty() || curr != null) {
+            if (curr != null) {
+                s.addFirst(curr);
+                curr = curr.left;
+            } else {
+                curr = s.removeFirst();
+                res.add(curr.data);
+                curr = curr.right;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(args);
+    }
+
+    private TreeInorder() {
+    }
+}
