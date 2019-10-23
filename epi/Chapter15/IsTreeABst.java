f91a2241e09b5d15cffd465c659ac6230675c5b3
nirvanarsc <nirvana_rsc@yahoo.com>
2019-10-24 08:54:52 +0900

    Chapter 15 Problem 1
    Optimized solutions

--- epi/Chapter15/IsTreeABst.java
+++ epi/Chapter15/IsTreeABst.java
@@ -1,5 +1,7 @@
 package epi.Chapter15;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 import epi.BinaryTreeNode;
@@ -9,8 +11,57 @@ import epi.utils.TestRunner;
 
 public final class IsTreeABst {
 
+    static class QueueEntry {
+        public BinaryTreeNode<Integer> treeNode;
+        public Integer left, right;
+
+        QueueEntry(BinaryTreeNode<Integer> treeNode, Integer left, Integer right) {
+            this.treeNode = treeNode;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     @EpiTest(testDataFile = "is_tree_a_bst.tsv")
     public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> tree) {
+        return validateBST(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public static boolean validateBST(BinaryTreeNode<Integer> tree, Integer left, Integer right) {
+        if (tree == null) {
+            return true;
+        }
+
+        if (tree.data < left || tree.data > right) {
+            return false;
+        }
+
+        return validateBST(tree.left, left, tree.data) && validateBST(tree.right, tree.data, right);
+    }
+
+    @EpiTest(testDataFile = "is_tree_a_bst.tsv")
+    public static boolean isBinaryTreeBST_BFS(BinaryTreeNode<Integer> tree) {
+        final Deque<QueueEntry> queue = new LinkedList<>();
+        queue.add(new QueueEntry(tree, Integer.MIN_VALUE, Integer.MAX_VALUE));
+
+        QueueEntry curr;
+        while (!queue.isEmpty()) {
+            curr = queue.pollFirst();
+            if (curr.treeNode != null) {
+                if (curr.treeNode.data < curr.left || curr.treeNode.data > curr.right) {
+                    return false;
+                }
+
+                queue.addLast(new QueueEntry(curr.treeNode.left, curr.left, curr.treeNode.data));
+                queue.addLast(new QueueEntry(curr.treeNode.right, curr.treeNode.data, curr.right));
+            }
+        }
+
+        return true;
+    }
+
+    @EpiTest(testDataFile = "is_tree_a_bst.tsv")
+    public static boolean isBinaryTreeBST_inOrder(BinaryTreeNode<Integer> tree) {
         if (tree == null) {
             return true;
         }
