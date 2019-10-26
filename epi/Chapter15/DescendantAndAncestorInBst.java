660715cfe652558de585bace065832eaf587ccc4
nirvanarsc <nirvana_rsc@yahoo.com>
2019-10-26 19:10:57 +0900

    Chapter 15 Problem 11

---
+++ epi/Chapter15/DescendantAndAncestorInBst.java
@@ -0,0 +1,73 @@
+package epi.Chapter15;
+
+import epi.BstNode;
+import epi.test_framework.BinaryTreeUtils;
+import epi.test_framework.EpiTest;
+import epi.test_framework.TimedExecutor;
+import epi.utils.TestRunner;
+
+public final class DescendantAndAncestorInBst {
+
+    public static boolean pairIncludesAncestorAndDescendantOfM(BstNode<Integer> possibleAncOrDesc0,
+                                                               BstNode<Integer> possibleAncOrDesc1,
+                                                               BstNode<Integer> middle) {
+        BstNode<Integer> search0 = possibleAncOrDesc0, search1 = possibleAncOrDesc1;
+
+        while (search0 != possibleAncOrDesc1 && search0 != middle
+               && search1 != possibleAncOrDesc0 && search1 != middle
+               && (search0 != null || search1 != null)) {
+            if (search0 != null) {
+                search0 = search0.data > middle.data ? search0.left : search0.right;
+            }
+            if (search1 != null) {
+                search1 = search1.data > middle.data ? search1.left : search1.right;
+            }
+        }
+
+        if (search0 == possibleAncOrDesc1 || search1 == possibleAncOrDesc0
+            || (search0 != middle && search1 != middle)) {
+            return false;
+        }
+
+        return search0 == middle ? searchTarget(middle, possibleAncOrDesc1)
+                                 : searchTarget(middle, possibleAncOrDesc0);
+    }
+
+    // You've passed 931/959 tests (28 tests fail where arg2 == arg4 or arg3 == arg4, contrary to problem definition.
+    public static boolean pairIncludesAncestorAndDescendantOfM2(BstNode<Integer> possibleAncOrDesc0,
+                                                                BstNode<Integer> possibleAncOrDesc1,
+                                                                BstNode<Integer> middle) {
+        if (possibleAncOrDesc0 == middle || possibleAncOrDesc1 == middle) {
+            return false;
+        }
+        if (searchTarget(possibleAncOrDesc0, middle) && searchTarget(middle, possibleAncOrDesc1)) {
+            return true;
+        }
+        return searchTarget(possibleAncOrDesc1, middle) && searchTarget(middle, possibleAncOrDesc0);
+    }
+
+    private static boolean searchTarget(BstNode<Integer> from, BstNode<Integer> target) {
+        while (from != null && from != target) {
+            from = from.data > target.data ? from.left : from.right;
+        }
+        return from == target;
+    }
+
+    @EpiTest(testDataFile = "descendant_and_ancestor_in_bst.tsv")
+    public static boolean pairIncludesAncestorAndDescendantOfMWrapper(TimedExecutor executor,
+                                                                      BstNode<Integer> tree,
+                                                                      int possibleAncOrDesc0,
+                                                                      int possibleAncOrDesc1,
+                                                                      int middle) throws Exception {
+        final BstNode<Integer> candidate0 = BinaryTreeUtils.mustFindNode(tree, possibleAncOrDesc0);
+        final BstNode<Integer> candidate1 = BinaryTreeUtils.mustFindNode(tree, possibleAncOrDesc1);
+        final BstNode<Integer> middleNode = BinaryTreeUtils.mustFindNode(tree, middle);
+        return executor.run(() -> pairIncludesAncestorAndDescendantOfM(candidate0, candidate1, middleNode));
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(args);
+    }
+
+    private DescendantAndAncestorInBst() {}
+}
