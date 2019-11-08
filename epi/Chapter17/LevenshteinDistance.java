c43d22e264dcfe4fe906a795c0ebf259dd221bae
nirvanarsc <nirvana_rsc@yahoo.com>
2019-11-08 18:29:36 +0100

    Chapter 17 Problem 2
    O(min(a,b)) space

--- epi/Chapter17/LevenshteinDistance.java
+++ epi/Chapter17/LevenshteinDistance.java
@@ -32,6 +32,24 @@ public final class LevenshteinDistance {
     }
 
     @EpiTest(testDataFile = "levenshtein_distance.tsv")
+    public static int levenshteinSpace(String a, String b) {
+        if (a.length() < b.length()) {
+            return levenshteinSpace(b, a);
+        }
+        final int[][] dp = new int[2][b.length() + 1];
+        for (int i = 0; i <= b.length(); i++) { dp[0][i] = i; }
+        for (int r = 1; r <= a.length(); r++) {
+            dp[r % 2][0] = r;
+            for (int c = 1; c <= b.length(); c++) {
+                final int cost = a.charAt(r - 1) == b.charAt(c - 1) ? 0 : 1;
+                dp[r % 2][c] = min(dp[(r - 1) % 2][c - 1] + cost, dp[(r - 1) % 2][c] + 1, dp[r % 2][c - 1] + 1);
+            }
+        }
+
+        return dp[a.length() % 2][b.length()];
+    }
+
+    @EpiTest(testDataFile = "levenshtein_distance.tsv")
     public static int levenshteinDistance(String a, String b) {
         final int[][] dp = new int[a.length() + 1][b.length() + 1];
         for (int i = 0; i <= a.length(); i++) { dp[i][0] = i; }
