9a20100686d74fb5a24658ef3a4db38fdaf276e5
nirvanarsc <nirvana_rsc@yahoo.com>
2019-11-23 00:43:52 +0900

    Chapter 17 Problem 3 & 4
    Recursive // Space optimised

--- epi/Chapter17/BinomialCoefficients.java
+++ epi/Chapter17/BinomialCoefficients.java
@@ -7,19 +7,55 @@ public final class BinomialCoefficients {
 
     @EpiTest(testDataFile = "binomial_coefficients.tsv")
     public static int computeBinomialCoefficient(int n, int k) {
-        final int[][] dp = new int[n + 1][n + 1];
-        for (int i = 0; i <= n; i++) {
+        final int[][] dp = new int[n + 1][k + 1];
+        for (int i = 1; i <= n; i++) {
             dp[i][0] = 1;
+        }
+        for (int i = 0; i <= k; i++) {
             dp[i][i] = 1;
         }
 
         for (int i = 2; i <= n; i++) {
-            for (int j = 1; j < n; j++) {
+            for (int j = 1; j <= k; j++) {
                 dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
             }
         }
 
         return dp[n][k];
+    }
+
+    @EpiTest(testDataFile = "binomial_coefficients.tsv")
+    public static int computeBinomialCoefficientSpace(int n, int k) {
+        final int[][] dp = new int[2][k + 2];
+        dp[0][0] = 1;
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + dp[(i - 1) % 2][j];
+            }
+        }
+
+        return dp[n % 2][k];
+    }
+
+    @EpiTest(testDataFile = "binomial_coefficients.tsv")
+    public static int computeBinomialCoefficientR(int n, int k) {
+        return calc(n, k, new int[n + 1][n + 1]);
+    }
+
+    public static int calc(int n, int k, int[][] cache) {
+        if (n == k || k == 0) {
+            return 1;
+        }
+
+        if (cache[n][k] != 0) {
+            return cache[n][k];
+        }
+
+        cache[n][k] = calc(n - 1, k - 1, cache) + calc(n - 1, k, cache);
+        return cache[n][k];
     }
 
     public static void main(String[] args) {
--- epi/Chapter17/NumberOfTraversalsMatrix.java
+++ epi/Chapter17/NumberOfTraversalsMatrix.java
@@ -1,5 +1,7 @@
 package epi.Chapter17;
 
+import java.util.Arrays;
+
 import epi.test_framework.EpiTest;
 import epi.utils.TestRunner;
 
@@ -9,12 +11,10 @@ public final class NumberOfTraversalsMatrix {
     public static int numberOfWays(int n, int m) {
         final int[][] dp = new int[n][m];
 
+        Arrays.fill(dp[0], 1);
+
         for (int i = 0; i < n; i++) {
             dp[i][0] = 1;
-        }
-
-        for (int i = 0; i < m; i++) {
-            dp[0][i] = 1;
         }
 
         for (int i = 1; i < n; i++) {
@@ -24,6 +24,47 @@ public final class NumberOfTraversalsMatrix {
         }
 
         return dp[n - 1][m - 1];
+    }
+
+    @EpiTest(testDataFile = "number_of_traversals_matrix.tsv")
+    public static int numberOfWaysSpace(int n, int m) {
+        if (n > m) {
+            return numberOfWaysSpace(m, n);
+        }
+
+        final int[][] dp = new int[n][2];
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int j = 1; j < m; j++) {
+            for (int i = 1; i < n; i++) {
+                dp[i][j % 2] = dp[i - 1][j % 2] + dp[i][(j - 1) % 2];
+            }
+        }
+
+        return dp[n - 1][(m - 1) % 2];
+    }
+
+    @EpiTest(testDataFile = "number_of_traversals_matrix.tsv")
+    public static int numWays(int n, int m) {
+        return calc(n - 1, m - 1, new int[n][m]);
+    }
+
+    public static int calc(int n, int m, int[][] cache) {
+        if (n == 0 || m == 0) {
+            return 1;
+        }
+
+        if (cache[n][m] != 0) {
+            return cache[n][m];
+        }
+
+        cache[n][m] = calc(n - 1, m, cache) + calc(n, m - 1, cache);
+
+        return cache[n][m];
     }
 
     public static void main(String[] args) {
