a2fe6cd8d485cb130b493794823042aeaee03fce
nirvanarsc <nirvana_rsc@yahoo.com>
2019-12-04 12:19:48 +0900

    Chapter 17 Problem 11
    Top Down

--- epi/Chapter17/PrettyPrinting.java
+++ epi/Chapter17/PrettyPrinting.java
@@ -1,15 +1,15 @@
 package epi.Chapter17;
 
-import java.util.Arrays;
-import java.util.List;
-
 import epi.test_framework.EpiTest;
 import epi.utils.TestRunner;
+
+import java.util.Arrays;
+import java.util.List;
 
 public final class PrettyPrinting {
 
     @EpiTest(testDataFile = "pretty_printing.tsv")
-    public static int minimumMessiness(List<String> words, int lineLength) {
+    public static int minimumMessinessBottomUp(List<String> words, int lineLength) {
         final int[] dp = new int[words.size()];
         Arrays.fill(dp, Integer.MAX_VALUE);
         int blanks = lineLength - words.get(0).length();
@@ -25,6 +25,41 @@ public final class PrettyPrinting {
             }
         }
         return dp[words.size() - 1];
+    }
+
+    @EpiTest(testDataFile = "pretty_printing.tsv")
+    public static int minimumMessinessTopDown(List<String> words, int lineLength) {
+        final int[] dp = new int[words.size()];
+        Arrays.fill(dp, -1);
+        return recurse(0, words, lineLength, dp);
+    }
+
+    private static int recurse(int start, List<String> words, int lineLength, int[] dp) {
+        if (start >= words.size()) {
+            return 0;
+        }
+
+        if (dp[start] != -1) {
+            return dp[start];
+        }
+
+        int res = Integer.MAX_VALUE;
+        for (int j = start + 1; j <= words.size(); j++) {
+            res = Math.min(res, recurse(j, words, lineLength, dp) + getBadness(start, j, words, lineLength));
+        }
+        dp[start] = res;
+        return dp[start];
+    }
+
+    private static int getBadness(int from, int to, List<String> words, int lineLength) {
+        int blanks = -1;
+        for (int i = from; i < to; i++) {
+            blanks += words.get(i).length() + 1;
+        }
+        if (blanks > lineLength) {
+            return (int) 1e9;
+        }
+        return (lineLength - blanks) * (lineLength - blanks);
     }
 
     public static void main(String[] args) {
